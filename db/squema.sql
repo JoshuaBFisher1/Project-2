51f9e68118c13886a1d4e5fd1aa96d2a69779522
Heidy Beltran <HeidyBeltran@Alains-MacBook-Air.local>
2023-07-17 18:52:24 -0400

    folders added, handlebars created

---
+++ db/squema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS harmonyhub_db;
+CREATE DATABASE harmonyhub_db;
--- script.js
+++ script.js
@@ -1,114 +1,117 @@
 // User interface
 function createUserInterface() {
-    // Code to create the user interface elements and handle user interactions
-    // For simplicity, let's assume you're using a browser environment and DOM manipulation
-  
-    // Create account form
-    const accountForm = document.createElement('form');
-    accountForm.addEventListener('submit', handleCreateAccount);
-    
-    const usernameInput = document.createElement('input');
-    usernameInput.type = 'text';
-    usernameInput.placeholder = 'Username';
-    
-    const passwordInput = document.createElement('input');
-    passwordInput.type = 'password';
-    passwordInput.placeholder = 'Password';
-    
-    const createAccountButton = document.createElement('button');
-    createAccountButton.textContent = 'Create Account';
-    
-    accountForm.appendChild(usernameInput);
-    accountForm.appendChild(passwordInput);
-    accountForm.appendChild(createAccountButton);
-  
-    // Create login form
-    const loginForm = document.createElement('form');
-    loginForm.addEventListener('submit', handleLogin);
-    
-    const loginUsernameInput = document.createElement('input');
-    loginUsernameInput.type = 'text';
-    loginUsernameInput.placeholder = 'Username';
-    
-    const loginPasswordInput = document.createElement('input');
-    loginPasswordInput.type = 'password';
-    loginPasswordInput.placeholder = 'Password';
-    
-    const loginButton = document.createElement('button');
-    loginButton.textContent = 'Log In';
-    
-    loginForm.appendChild(loginUsernameInput);
-    loginForm.appendChild(loginPasswordInput);
-    loginForm.appendChild(loginButton);
-  
-    // Add elements to the page
-    document.body.appendChild(accountForm);
-    document.body.appendChild(loginForm);
+  // Code to create the user interface elements and handle user interactions
+  // For simplicity, let's assume you're using a browser environment and DOM manipulation
+
+  // Create account form
+  const accountForm = document.createElement("form");
+  accountForm.addEventListener("submit", handleCreateAccount);
+
+  const usernameInput = document.createElement("input");
+  usernameInput.type = "text";
+  usernameInput.placeholder = "Username";
+
+  const passwordInput = document.createElement("input");
+  passwordInput.type = "password";
+  passwordInput.placeholder = "Password";
+
+  const createAccountButton = document.createElement("button");
+  createAccountButton.textContent = "Create Account";
+
+  accountForm.appendChild(usernameInput);
+  accountForm.appendChild(passwordInput);
+  accountForm.appendChild(createAccountButton);
+
+  // Create login form
+  const loginForm = document.createElement("form");
+  loginForm.addEventListener("submit", handleLogin);
+
+  const loginUsernameInput = document.createElement("input");
+  loginUsernameInput.type = "text";
+  loginUsernameInput.placeholder = "Username";
+
+  const loginPasswordInput = document.createElement("input");
+  loginPasswordInput.type = "password";
+  ``;
+  loginPasswordInput.placeholder = "Password";
+
+  const loginButton = document.createElement("button");
+  loginButton.textContent = "Log In";
+
+  loginForm.appendChild(loginUsernameInput);
+  loginForm.appendChild(loginPasswordInput);
+  loginForm.appendChild(loginButton);
+
+  // Add elements to the page
+  document.body.appendChild(accountForm);
+  document.body.appendChild(loginForm);
+}
+
+// Account creation and authentication
+function createAccount(username, password) {
+  // Code to create a new user account
+  // You can store the user account information in a database or local storage
+  // For simplicity, let's assume we store the account in an array
+
+  const userAccount = {
+    username,
+    password,
+  };
+
+  // Add the user account to the array or database
+  // Example:
+  userAccounts.push(userAccount);
+}
+
+function login(username, password) {
+  // Code to authenticate the user and log them into the app
+  // You can validate the username and password against the stored user account information
+  // For simplicity, let's assume we have the userAccounts array with stored accounts
+
+  const userAccount = userAccounts.find(
+    (account) => account.username === username && account.password === password
+  );
+
+  if (userAccount) {
+    // User is authenticated, proceed with app functionality
+    // For simplicity, let's assume we call a function to handle successful login
+    handleSuccessfulLogin(userAccount);
+  } else {
+    // Authentication failed, display an error message
+    // For simplicity, let's assume we have a function to display error messages on the UI
+    displayErrorMessage("Invalid username or password. Please try again.");
   }
-  
-  // Account creation and authentication
-  function createAccount(username, password) {
-    // Code to create a new user account
-    // You can store the user account information in a database or local storage
-    // For simplicity, let's assume we store the account in an array
-  
-    const userAccount = {
-      username,
-      password
-    };
-  
-    // Add the user account to the array or database
-    // Example:
-    userAccounts.push(userAccount);
-  }
-  
-  function login(username, password) {
-    // Code to authenticate the user and log them into the app
-    // You can validate the username and password against the stored user account information
-    // For simplicity, let's assume we have the userAccounts array with stored accounts
-  
-    const userAccount = userAccounts.find(account => account.username === username && account.password === password);
-  
-    if (userAccount) {
-      // User is authenticated, proceed with app functionality
-      // For simplicity, let's assume we call a function to handle successful login
-      handleSuccessfulLogin(userAccount);
-    } else {
-      // Authentication failed, display an error message
-      // For simplicity, let's assume we have a function to display error messages on the UI
-      displayErrorMessage('Invalid username or password. Please try again.');
-    }
-  }
-  
-  // Event handlers
-  function handleCreateAccount(event) {
-    event.preventDefault();
-  
-    const username = event.target.elements.username.value;
-    const password = event.target.elements.password.value;
-  
-    createAccount(username, password);
-  
-    // Clear the form fields
-    event.target.reset();
-  
-    // Display a success message or navigate to the login screen
-  }
-  
-  function handleLogin(event) {
-    event.preventDefault();
-  
-    const username = event.target.elements.username.value;
-    const password = event.target.elements.password.value;
-  
-    login(username, password);
-  
-    // Clear the form fields
-    event.target.reset();
-  
-    // Display a success message or navigate to the main app screen
-  }
-  
-  // Entry point
-  // This is where the execution of the app would start
-  createUserInterface();
+}
+
+// Event handlers
+function handleCreateAccount(event) {
+  event.preventDefault();
+
+  const username = event.target.elements.username.value;
+  const password = event.target.elements.password.value;
+
+  createAccount(username, password);
+
+  // Clear the form fields
+  event.target.reset();
+
+  // Display a success message or navigate to the login screen
+}
+
+function handleLogin(event) {
+  event.preventDefault();
+
+  const username = event.target.elements.username.value;
+  const password = event.target.elements.password.value;
+
+  login(username, password);
+
+  // Clear the form fields
+  event.target.reset();
+
+  // Display a success message or navigate to the main app screen
+}
+
+// Entry point
+// This is where the execution of the app would start
+createUserInterface();
